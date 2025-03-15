e58b58a2f2046382bbcd408620de318aadd15ba4
Abanoub Samir <abanoub.sabry@evapharma.com>
2025-03-15 02:49:57 +0200

    app creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./views/layout/header/header.component";
+import { FooterComponent } from "./views/layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
