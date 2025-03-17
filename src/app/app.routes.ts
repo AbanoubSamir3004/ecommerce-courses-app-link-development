e5ea9490a11ac5e4668b79e4f62feac5c68fbada
Abanoub Samir <abanoub.sabry@evapharma.com>
2025-03-17 03:55:11 +0200

    cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: 'home',
     loadComponent: () => import('./views/pages/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./views/pages/cart/cart.component').then(m => m.CartComponent)
+  },
+  { path: '**', redirectTo: '/home' }
 
-  }
 ];
